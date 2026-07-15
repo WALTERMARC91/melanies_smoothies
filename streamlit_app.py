6ee5b7c68355aea0bb012bf5cb2013eb9fbdcf23
WALTERMARC91 <marc.walter@isoskele.fr>
2026-07-15 11:53:13 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,4 +54,4 @@ import requests
 smoothiefroot_response = requests.get(
     "https://my.smoothiefroot.com/api/fruit/watermelon"
 )
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
